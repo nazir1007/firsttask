25d1a454d664d1590b747eeb6037ed196fe07cec
Mohd Nazir <mohdnazir1007@gmail.com>
2024-01-28 22:41:01 +0530

    some changes in views.py file

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def task_view(request):
-    first_name = "Mohd"
-    last_name = "Nazir"
-    return render(request, "task.html", {'first_name': first_name, 'last_name': last_name})
+   context = {
+       "first_name": "Mohd",
+       "last_name": "Nazir",
+    }
+   return render(request, "task.html", context)
